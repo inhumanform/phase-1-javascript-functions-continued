e981a66d0c78f48d331d6fd88cdf3e8b11ff2995
Drew <dch528@gmail.com>
2024-01-30 09:12:19 -0500

    Drew Hairston Functions Continued Lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+
+  function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+
+  function wrapAdjective(symbol = "*") {
+    return function(adjective) {
+      return `You are ${symbol}${adjective}${symbol}!`;
+    }
+  }
+
+  wrapAdjective("%")("a dedicated programmer");
+  let emphaticPhrase = highlight("a dedicated programmer");
+  console.log(emphaticPhrase);
